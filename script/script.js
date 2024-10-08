b483aded92863bad61fc985835db31486f3cb30b
TofusAndPancakes <christoferalvin.work@gmail.com>
2024-10-08 21:00:55 +0100

    Added filters.

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function (event) {
+    //Navbar System
     navTrigger = document.getElementById("navTrigger");
 
     navBar = document.getElementById("navActive");
@@ -60,6 +61,74 @@ document.addEventListener("DOMContentLoaded", function (event) {
         deactivateNavBarSmall();
     });
 
+    //Filter System (W3Schools Slight Modified)
+    function filterSelection (filterTag) {
+        console.log("now filter" + filterTag);
+
+        var x;
+        x = document.getElementsByClassName("workEntry");
+
+        if (filterTag == "all"){ 
+            filterTag = "";
+        };
+        for (let i = 0; i < x.length; i++){
+            
+            filterRemove(x[i], "show");
+            
+            if (filterTag === "") {
+                filterAdd(x[i], "show");
+            }  else if (x[i].className.indexOf(filterTag) > -1){
+                filterAdd(x[i], "show");
+            }
+        }
+    }
+
+    function filterAdd (element, name) {
+        var array1, array2;
+
+        array1 = element.className.split(" ");
+        array2 = name.split(" ");
+
+        for (let i = 0; i < array2.length; i++) {
+            if (array1.indexOf(array2[i]) == -1){
+                element.className = element.className + " " + array2[i];
+            };
+        }
+
+    }
+
+    function filterRemove (element, name) {
+        var array1, array2;
+
+        array1 = element.className.split(" ");
+        array2 = name.split(" ");
+        
+        for (let i = 0; i < array2.length; i++) {
+            while (array1.indexOf(array2[i]) > -1) {
+                array1.splice(array1.indexOf(array2[i]), 1);
+            };
+        }
+        element.className = array1.join(" ");
+    }
+
+    //Setting Up Button Event Listener
+    var buttonContainer =  document.getElementById("introductionTagList");
+    var buttonArray = buttonContainer.getElementsByClassName("introductionTag");
+
+    for (let i = 0; i < buttonArray.length; i++){
+        buttonArray[i].addEventListener("click", () =>{
+            var filterTagFromButton = buttonArray[i].dataset.filterTag;
+            console.log("FilterTag " + buttonArray[i].dataset + " " + filterTagFromButton);
+            filterSelection(filterTagFromButton);
+
+            var currentActiveButton = document.getElementsByClassName("filterActive");
+            currentActiveButton[0].classList.remove("filterActive");
+            buttonArray[i].classList.add("filterActive");
+        });
+
+    }
+
+    filterSelection("all");
 });
 
 //Credit: https://stackoverflow.com/questions/9899372/vanilla-javascript-equivalent-of-jquerys-ready-how-to-call-a-function-whe
